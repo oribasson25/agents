59a79d61ba22440561441660c8adaf18cd2c59d2
orikibasson-coder <oriki.basson@gmail.com>
2026-04-14 09:31:04 +0300

    Fix: remove duplicate CSP headers, set frame-ancestors only in widget route

--- api/widget/[agentId].js
+++ api/widget/[agentId].js
@@ -141,6 +141,7 @@ async function sendMsg(){
 
   res.setHeader('Content-Type', 'text/html; charset=utf-8');
   res.setHeader('X-Frame-Options', 'ALLOWALL');
-  res.setHeader('Content-Security-Policy', "frame-ancestors * file:;");
+  res.setHeader('Content-Security-Policy', "frame-ancestors * file: data:;");
+  res.setHeader('Cache-Control', 'no-store');
   res.send(html);
 }
